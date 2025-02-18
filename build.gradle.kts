be61975e88bfdcccbe5a51195a90732fa56437da
yuhojae <ho_0214@naver.com>
2025-02-18 22:47:12 +0900

    feat : cicd test

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,13 +49,13 @@ subprojects {
         useJUnitPlatform()
     }
 
-    tasks.bootJar {
-        archiveBaseName.set("embitips")  // JAR 이름
-        archiveVersion.set("0.0.1")      // 버전
-        mainClass.set("com.baekjoon.MbtipsApplication")
-    }
-    tasks.jar {
-        enabled = false
-    }
+}
 
+tasks.bootJar {
+    archiveBaseName.set("embitips")  // JAR 이름
+    archiveVersion.set("0.0.1")      // 버전
+    mainClass.set("com.baekjoon.MbtipsApplication")
+}
+tasks.jar {
+    enabled = false
 }
