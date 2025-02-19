e638ba946046f3159960db5da94eefdddc5a559f
yuhojae <ho_0214@naver.com>
2025-02-20 01:18:09 +0900

    feat : 엔티티 생성

---
+++ repository/src/main/java/com/embitips/message/Message.java
@@ -0,0 +1,44 @@
+package com.embitips.message;
+
+import com.embitips.conversation.Conversation;
+import com.embitips.user.User;
+import com.embitips.virtualfriend.VirtualFriend;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "message")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "virtual_friend_id", nullable = false)
+    private VirtualFriend virtualFriend;
+
+    @Column(length = 500, nullable = false)
+    private String messageContent;
+
+    @Column(nullable = false)
+    private LocalDateTime
+            sentAt;
+
+    @Column(nullable = false)
+    private Boolean isRead;
+
+}
---
+++ repository/src/main/java/com/embitips/user/User.java
@@ -0,0 +1,21 @@
+package com.embitips.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 30)
+    private String userEmail;
+}
+
