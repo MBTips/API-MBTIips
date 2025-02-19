eeb7a7e420f9f92eccf786caeacd9f9f11ddd1f9
bumoo <a43727702@gmail.com>
2025-02-19 22:50:04 +0900

    feat : L7 HealthCheck 추가

---
+++ api/src/main/java/com/mbtips/common/controller/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.mbtips.common.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/health-check")
+@Tag(name = "L7 health-check", description = "L7 health-check API")
+public class HealthCheckController {
+
+    @GetMapping
+    @Operation(summary = "L7 Health Check", description = "L7 Health Check API")
+    public String healthCheck() {
+        return "ok";
+    }
+}
